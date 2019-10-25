04f9d0904922e2fe95e237ac77cbf3936c68fe68
Hugo V <eyleteam@gmail.com>
2019-10-25 10:47:10 +0200

    modification suite a la demande sur la PR

--- heroesfighter/src/Components/ArenaFight.jsx
+++ heroesfighter/src/Components/ArenaFight.jsx
@@ -2,27 +2,25 @@ import React from 'react';
 import './ArenaFight.css';
 import Card from './Card.jsx';
 
-class ArenaFight extends React.Component {
-  render() {
-    return (
-      <div className="arena_box">
-        <div className="card_versus">
-          <Card className="Card" />
-          <p className="versus_text">VS</p>
-          <Card />
-        </div>
-        <div className="button_box">
-          <button className="bonusButtonLightning" aria-label="Save" type="button" />
-          <button className="bonusButtonBoxingGlove" aria-label="Save" type="button" />
-          <button className="bonusButtonBook" aria-label="Save" type="button" />
-        </div>
-        <div className="fightButton_box">
-          <button className="fightButton" type="button">FIGHT</button>
-        </div>
+function ArenaFight () {
+  return (
+    <div className="arena_box">
+      <div className="card_versus">
+        <Card className="Card" />
+        <p className="versus_text">VS</p>
+        <Card />
       </div>
-
-    );
-  }
+      <div className="button_box">
+        <button className="bonusButtonLightning" aria-label="Save" type="button" />
+        <button className="bonusButtonBoxingGlove" aria-label="Save" type="button" />
+        <button className="bonusButtonBook" aria-label="Save" type="button" />
+      </div>
+      <div className="fightButton_box">
+        <button className="fightButton" type="button">FIGHT</button>
+      </div>
+    </div>
+  );
 }
+
 
 export default ArenaFight;
