9e3e90bb9b154f5883195c65c19765d0bcc3bc5b
Victor Biard <vi.biard@gmail.com>
2019-11-06 17:37:53 +0100

    add a Main component Css and Jsx

--- heroesfighter/src/App.jsx
+++ heroesfighter/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Home from './Components/Home';
+import Main from './Components/Main';
 
 function App() {
   return (
     <div className="App">
+      <Main />
       <Home />
     </div>
   );
---
+++ heroesfighter/src/Components/Main.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Main.css'
+
+class Main extends React.Component {
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <div className = 'MainBackground'>
+                <div className = 'BoxButtonImg'>
+                    <div className = 'BoxImg'>
+                        <img className = 'ImgCaptainSuperman' src="https://image.noelshack.com/fichiers/2019/45/3/1573054297-superman-transparent-png-379901.png" alt=""/>
+                        <img src="https://image.noelshack.com/fichiers/2019/45/3/1573052628-heroes-fighter.png" alt=""/>
+                        <img className = 'ImgCaptainAmerica' src="https://image.noelshack.com/fichiers/2019/45/3/1573057748-comic-captain-america-transparent-png-599277.png" alt=""/>
+                    </div>
+                    <div>
+                        <button className = 'EnterButton'>ENTER</button>
+                    </div>
+                </div>      
+            </div>
+        )
+    }
+}
+
+export default Main 
